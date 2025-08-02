cc8e4edf0e590e64a81f295060fe216c677ca46f
MiRene15 <ladymirene@proton.me>
2025-08-02 12:18:16 +0800

    Minor Adjustments in LibraryController.cs

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -75,26 +75,106 @@ namespace Library_Management_System.Controllers
                 IsAvailable = true
             };
 
+            var book6 = new Book
+            {
+                Author = author6,
+                Title = "The Wonderful Wizard of Oz",
+                Genre = Genre.Kids,
+                ISBN = "978-971-10-1000-6",
+                PublicationYear = 1900,
+                DateReturned = DateTime.Now.AddDays(2),
+                IsAvailable = true
+            };
+
+            var book7 = new Book
+            {
+                Author = author7,
+                Title = "The Hobbit",
+                Genre = Genre.Fanfiction,
+                ISBN = "978-971-10-1000-7",
+                PublicationYear = 1937,
+                DateReturned = DateTime.Now.AddDays(3),
+                IsAvailable = true
+            };
+
+            var book8 = new Book
+            {
+                Author = author8,
+                Title = "1984",
+                Genre = Genre.Scifi,
+                ISBN = "978-971-10-1000-8",
+                PublicationYear = 1949,
+                DateReturned = DateTime.Now.AddDays(1),
+                IsAvailable = true
+            };
+
+            var book9 = new Book
+            {
+                Author = author9,
+                Title = "And Then There Were None",
+                Genre = Genre.Horror,
+                ISBN = "978-971-10-1000-9",
+                PublicationYear = 1939,
+                DateReturned = DateTime.Now.AddDays(4),
+                IsAvailable = true
+            };
+
+            var book10 = new Book
+            {
+                Author = author10,
+                Title = "The Adventures of Tom Sawyer",
+                Genre = Genre.Kids,
+                ISBN = "978-971-10-1000-10",
+                PublicationYear = 1876,
+                DateReturned = DateTime.Now.AddDays(6),
+                IsAvailable = true
+            };
+
 
             var user1 = new User { Id = 1, FullName = "Juan Dela Cruz" };
             var user2 = new User { Id = 2, FullName = "Maria Clara" };
             var user3 = new User { Id = 3, FullName = "Jose Rizal" };
             var user4 = new User { Id = 4, FullName = "Andres Bonifacio" };
             var user5 = new User { Id = 5, FullName = "Emilio Aguinaldo" };
+            var user6 = new User { Id = 6, FullName = "Andres Bonifacio" };
+            var user7 = new User { Id = 7, FullName = "Emilio Jacinto" };
+            var user8 = new User { Id = 8, FullName = "Apolinario Mabini" };
+            var user9 = new User { Id = 9, FullName = "Gregoria de Jesus" };
+            var user10 = new User { Id = 10, FullName = "Jose Palma" };
 
             user1.BorrowedBooks.Add(book1);
+            user2.BorrowedBooks.Add(book2);
+            user3.BorrowedBooks.Add(book3);
+            user4.BorrowedBooks.Add(book4);
+            user5.BorrowedBooks.Add(book5);
+            user6.BorrowedBooks.Add(book6);
+            user7.BorrowedBooks.Add(book7);
+            user8.BorrowedBooks.Add(book8);
+            user9.BorrowedBooks.Add(book9);
+            user10.BorrowedBooks.Add(book10);
 
             library.Books.Add(book1);
             library.Books.Add(book2);
             library.Books.Add(book3);
             library.Books.Add(book4);
             library.Books.Add(book5);
+            library.Books.Add(book6);
+            library.Books.Add(book7);
+            library.Books.Add(book8);
+            library.Books.Add(book9);
+            library.Books.Add(book10);
+
 
             library.Users.Add(user1);
             library.Users.Add(user2);
             library.Users.Add(user3);
             library.Users.Add(user4);
-            library.Users.Add(user5);     
+            library.Users.Add(user5);   
+            library.Users.Add(user6);
+            library.Users.Add(user7);
+            library.Users.Add(user8);
+            library.Users.Add(user9);
+            library.Users.Add(user10);
         }
 
 
